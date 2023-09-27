a96b0d2fd8c8f2af346ff08e13f11c8bbf43a546
Kunal Rawat <kunalrawat7766@gmail.com>
2023-09-27 11:26:52 +0530

    more patterns practiced 2

--- Basics/6 More Pattern Problem.cpp
+++ Basics/6 More Pattern Problem.cpp
@@ -45,4 +45,42 @@ int main() {
         cout << endl;
         spaces-=2;
     }
+    cout << endl;
+
+    for (int i = 1; i <= n; i++) {
+        char SomeChar1 = 'A';
+        for (int j = 1; j <= n-i+1; j++) {
+            cout << SomeChar1 << " ";
+            SomeChar1++;
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+
+    for (int i = 1; i <= n; i++) {
+        // space
+        for (int j = 1; j <= n-i; j++) {
+            cout << " ";
+        }
+        // char
+        char SomeChar1 = 'A';
+        int breakpoint = (2*i-1)/2;
+        for (int j = 1; j <= 2*i-1 ; j++) {
+            if(j <= breakpoint) {
+                cout << SomeChar1;
+                SomeChar1++;
+            }
+            else{
+                cout << SomeChar1;
+                SomeChar1--;
+            }
+        }
+        // number
+        for (int j = 1; j <= n-i; j++) {
+            cout << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
 }
---
+++ Basics/Practice/practice file 8.cpp
@@ -0,0 +1,99 @@
+//First Pattern: Take an input n from the user, and create a pattern like below, for n=5, we have output like this.
+//
+//1
+//2 1
+//3 2 1
+//4 3 2 1
+//5 4 3 2 1
+//
+//Second Pattern: Take an input n from the user, and create a pattern like below, for n=5, we have output like this.
+//
+//A
+//B B
+//C C C
+//D D D D
+//E E E E E
+//
+//Third Pattern: Take an input n from the user, and create a pattern like below, for n=5, we have output like this.
+//
+//5
+//5 4
+//5 4 3
+//5 4 3 2
+//5 4 3 2 1
+//
+//Fourth Pattern: Take an input n from the user, and create a pattern like below, for n=5, we have output like this.
+//
+//E
+//E D
+//E D C
+//E D C B
+//E D C B A
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter the number n: " << endl;
+    cin >> n;
+/*----------------------------1st----------------------------*/
+    for (int i = 1; i <= n; i++) {
+        // spaces
+        for (int j = 1; j <= n-i; j++) {
+            cout << "  ";
+        }
+        // numbers
+        for (int j = i; j >= 1; j--) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+/*----------------------------2nd--------------------------*/
+    char SomeChar1 = 'A';
+    for (int i = 1; i <= n; i++) {
+        // spaces
+        for (int j = 1; j <= n-i; j++) {
+            cout << "  ";
+        }
+        // char
+        for (int j = 1; j <= i; j++) {
+            cout << SomeChar1 << " ";
+        }
+        cout << endl;
+        SomeChar1++;
+    }
+    cout << endl;
+
+/*----------------------------3rd-------------------------*/
+    for (int i = 1; i <= n; i++) {
+        // spaces
+        for (int j = 1; j <= n-i; j++) {
+            cout << "  ";
+        }
+        // numbers
+        for (int j = n; j >= n-i+1; j--) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+/*----------------------------4th-------------------------*/
+    for (int i = 1; i <= n; i++) {
+        // spaces
+        for (int j = 1; j <= n-i; j++) {
+            cout << "  ";
+        }
+        // char
+        char SomeChar2 = 'E';
+        for (int j = 1; j <= i; j++) {
+            cout << SomeChar2 << " ";
+            SomeChar2--;
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+}
